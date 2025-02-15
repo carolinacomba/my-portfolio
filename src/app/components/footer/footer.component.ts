bcef6d82b7b206e51548b4cc3805d7cc2135236b
Carolina Comba <111009258+carolinacomba@users.noreply.github.com>
2025-02-15 00:14:21 -0300

    cambios y cositas nuevas

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  
+}
